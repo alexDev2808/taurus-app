194b3196a435671e072e6a4a1306a762dcc7f864
tauesp127-alexis <gestionti@taurus.com.mx>
2024-12-09 08:46:49 -0600

    Home y Sidebar component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SharedModule } from '../shared/shared.module';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     ButtonModule,
+    CardModule,
     SharedModule
   ],
   templateUrl: './home.component.html',
